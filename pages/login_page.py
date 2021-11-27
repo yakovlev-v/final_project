4ed1a5c97b47abb6cbd50092273ed32da42d1808
yakovlev-v <61628407+yakovlev-v@users.noreply.github.com>
2021-11-27 22:39:50 +0500

    Добавил проверки из 8 шага урока 4.2: - добавил новый PageObject - login_page, в нем описываются проверки - добавил новые локаторы для формы регистрации и формы логина - добавил новые тесты, но они пока не проходят, возможно в след. уроке будет объяснено, что надо добавить

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import MainPageLocators, LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert 'login' in self.browser.current_url, "Подстроки login нет в данном URL"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.browser.find_element(*LoginPageLocators, LOGIN_FORM), "Формы логина на странице нет"
+
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.browser.find_element(*LoginPageLocators, REGISTRATION_FORM), "Формы регистрации на странице нет"
