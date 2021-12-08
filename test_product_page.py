ee990d4c95549598b1dbe83d8e1d1236954d6e7f
yakovlev-v <61628407+yakovlev-v@users.noreply.github.com>
2021-12-08 23:49:21 +0500

    поправил маркировку в test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -54,6 +54,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -63,7 +64,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_form()
 
 
-@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = MainPage(browser, link)
